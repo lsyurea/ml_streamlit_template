e83175407f04547c97c7b8f982ee69ea5479946c
Lee Si Yuan <lee.si.yuan.20@gmail.com>
2024-12-11 16:01:38 +0800

    Update code

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # ml_streamlit_template
 
-Docker does not work, please run the code directly on your local machine.
-This is because docker does not handle camera access during virtualisation
-
 The command to run directly on local machine is `streamlit run app.py --server.port=8501` or `streamlit run app.py`
--- app.py
+++ app.py
@@ -1,46 +1,55 @@
-import time
-
-import cv2
+'''
+This script defines the UI for Streamlit app
+'''
 import streamlit as st
 import torch
+import cv2
+from streamlit_webrtc import webrtc_streamer
 from PIL import Image
+import av
+
+# from videotransformer import VideoTransformer
 
 
-# Load YOLOv9 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-st.title("YOLOv8 People Counter")
-
-# Start video capture
-cap = cv2.VideoCapture(0)
-time.sleep(2)  # Allow the camera to initialize
-
-if not cap.isOpened():
-    st.error("Failed to open camera")
-else:
-    # Create placeholders for video and count
-    stframe = st.empty()
-    count_text = st.text("Number of people detected: 0")
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            st.error(f"Failed to capture image from camera")
-            break
-            
-        # Convert frame to PIL image
-        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        
-        # Perform detection
+def boundingbox_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    # Convert frame to PIL image
+    image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))    
+    # Perform detection
+    with torch.amp.autocast('cuda'):
         results = model(image)
-        
-        # Filter results for people (class 0 in COCO dataset)
-        people = results.xyxy[0][results.xyxy[0][:, -1] == 0]
-        
-        # Draw bounding boxes and count people
-        count = len(people)
-        for *box, conf, cls in people:
-            cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
-            
-        # Update display
-        stframe.image(frame, channels="BGR")
-        count_text.text(f"Number of people detected: {count}")
+    # Filter results for people (class 0 in COCO dataset)
+    people = results.xyxy[0][results.xyxy[0][:, -1] == 0]
+    # Draw bounding boxes and count people
+    st.session_state.num_people = len(people)
+    for *box, conf, cls in people:
+        cv2.rectangle(img,
+                      (int(box[0]),
+                       int(box[1])),
+                      (int(box[2]),
+                       int(box[3])),
+                      (0, 255, 0), 2)
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+st.title("Track people in a video stream")
+
+if 'num_people' not in st.session_state:
+    st.session_state.num_people = 0
+
+rtc_configuration = {
+    "iceServers": [
+        {"urls": "stun.l.google.com:19302"},
+    ]
+}
+
+num_people_placeholder = st.empty()
+
+webrtc_streamer(
+    key="example",
+    video_frame_callback=boundingbox_frame_callback,
+    media_stream_constraints={"video": True, "audio": False}
+)
+
+# num_people_placeholder.write(f"Number of people detected: {st.session_state.num_people}")
